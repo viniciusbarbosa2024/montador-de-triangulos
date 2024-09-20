6a1652545c23ee9e27134197a66c475d2d7c68d0
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-19 21:31:47 -0300

    iniando js + indicação de vértices

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const sideAB = document.getElementById('sideAB')
+const sideBC = document.getElementById('sideBC')
+const sideCA = document.getElementById('sideCA')
+const assemble = document.getElementById('assemble')
+
+assemble.addEventListener('click',generalFunction)
+
+function generalFunction() {
+    if () {
+
+    } else {
+        alert('Não é possível formar um triângulo com estas medidas')
+    }
+}

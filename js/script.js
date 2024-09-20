9aaf4b603bb6454fb2e19aefefca2bdee2c78ce3
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-19 21:57:37 -0300

    Determinando cossenos

--- js/script.js
+++ js/script.js
@@ -5,11 +5,7 @@ const assemble = document.getElementById('assemble')
 
 assemble.addEventListener('click',generalFunction)
 
-function conditionsOfExistenceOfATriangle() {
-    const sideAB = Number(sideAB_HTML.value)
-    const sideBC = Number(sideBC_HTML.value)
-    const sideCA = Number(sideCA_HTML.value)
-
+function conditionsOfExistenceOfATriangle(sideAB,sideBC,sideCA) {
     if ((sideAB < sideBC + sideCA) && (sideBC < sideAB + sideCA) && (sideCA < sideBC + sideAB)) {
         return true
     } else {
@@ -18,8 +14,20 @@ function conditionsOfExistenceOfATriangle() {
 }
 
 function generalFunction() {
-    if (conditionsOfExistenceOfATriangle()) {
-        alert()    
+    const sideAB = Number(sideAB_HTML.value)
+    const sideBC = Number(sideBC_HTML.value)
+    const sideCA = Number(sideCA_HTML.value)
+    
+    if (conditionsOfExistenceOfATriangle(sideAB,sideBC,sideCA)) {
+        //Por lei dos cossenos
+
+        const cosineAngleA = (sideCA**2+sideAB**2 - sideBC**2) /(2*sideCA*sideAB) 
+
+        const cosineAngleB = (sideBC**2+sideAB**2 - sideCA**2) /(2*sideBC*sideAB) 
+
+        const cosineAngleC = (sideBC**2+sideCA**2 - sideAB**2) /(2*sideBC*sideCA) 
+        
+        
     } else {
         alert('Não é possível formar um triângulo com estas medidas')
     }
